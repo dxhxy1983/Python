e77a2c9302706f7704de26a771c154dd4c13a8ee
dxhxy <dxhxy1983@foxmail.com>
2019-11-01 16:31:29 +0800

    最大子列和初始

---
+++ 数据结构/最大子列和.py
@@ -0,0 +1,18 @@
+def MaxSubseqSum4(A, N ):
+    
+    ThisSum = MaxSum = 0
+    for i in range(0,N):
+          ThisSum += A[i] /* 向右累加 */
+          if( ThisSum > MaxSum ):
+                  MaxSum = ThisSum /* 发现更大和则更新当前结果 */
+          else if( ThisSum < 0 ): /* 如果当前子列和为负 */
+                  ThisSum = 0 /* 则不可能使后面的部分和增大，抛弃之 */
+    }
+    return MaxSum 
+def main():
+    Num=eval(input())
+    rawInput=input()
+    Arry[]=rawInput.split(' ')
+    MaxSubseqSum=MaxSubseqSum4(Arry,Num)
+    print(MaxSubseqSum)
+
