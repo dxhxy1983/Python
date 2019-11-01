6c51806a06cd97239a4e8e7c8b21c105a80a51fd
dxhxy <dxhxy1983@foxmail.com>
2019-11-01 17:11:57 +0800

    全部正确

--- 数据结构/最大子列和.py
+++ 数据结构/最大子列和.py
@@ -2,10 +2,10 @@ def MaxSubseqSum4(A, N ):
     
     ThisSum = MaxSum = 0
     for i in range(0,N):
-          ThisSum += A[i] #/* 向右累加 *
+          ThisSum += eval(A[i]) #/* 向右累加 *
           if( ThisSum > MaxSum ):
                   MaxSum = ThisSum #/* 发现更大和则更新当前结果 */
-          elsif( ThisSum < 0 ): #/* 如果当前子列和为负 */
+          elif( ThisSum < 0 ): #/* 如果当前子列和为负 */
                   ThisSum = 0 #/* 则不可能使后面的部分和增大，抛弃之 */
     
     return MaxSum 
