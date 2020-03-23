8b88bb15bccd6e6326c5a2c4c5340a3ef375d6d0
dxhxy <dxhxy1983@foxmail.com>
2020-03-23 11:53:57 +0800

    结果正确

--- 谁没提交.py
+++ 谁没提交.py
@@ -1,10 +1,14 @@
 import jieba
-namelist=open('C:\\Users\\D\\Documents\\Python\\namelist.txt', 'r').read()
+allstudents=open('C:\\Users\\D\\Documents\\Python\\学生名单.txt', 'r').read()
+allList=allstudents.split()
+jieba.load_userdict(allList)
+namelist=open('C:\\Users\\D\\Documents\\Python\\接龙内容.txt', 'r').read()
+namelist=jieba.lcut(namelist)
 
-allstudents=open('C:\\Users\\D\\Documents\\Python\\allstudents.txt', 'r').read()
-
-for keyword in allstudents:
-    jieba.add_word(keyword)
-jieba.load_userdict
-word=jieba.lcut(namelist)
-print(word)
+inList=[]
+for keyword in allList:
+    if keyword in namelist:
+        inList.append(keyword)
+print(namelist)
+b=set(allList)-set(inList)
+print(b)
