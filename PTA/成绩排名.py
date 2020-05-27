b9f2a96bf0746cefd3f9ef9f1b151ef458d3bd9e
dxhxy <dxhxy1983@foxmail.com>
2020-05-27 13:22:56 +0800

    DataFramef方法完成,但超时

--- PTA/成绩排名.py
+++ PTA/成绩排名.py
@@ -15,12 +15,16 @@ if __name__ == "__main__":
 
     s = keyinput()
     l=[]
-    d={}
+    a = ['姓名', '学号','成绩']
+    df = pd.DataFrame(index=a) #将输入转换为DataFrame
     for i in range(len(s)):
         l=s[i].split()
-        a = ['姓名', '学号','成绩']
-        dict1=dict(zip(a,l))
-        d.update(dict1)
-    df=pd.DataFrame(d)
-    print(df)
+        df0=pd.Series(l,index=a)
+        df[i]=df0
 
+    dft=df.T
+    dfmax=dft[dft['成绩']==dft['成绩'].max()]
+    dfmin=dft[dft['成绩']==dft['成绩'].min()]
+
+    print('{0} {1}'.format(dfmax.iloc[0,0],dfmax.iloc[0,1]))
+    print('{0} {1}'.format(dfmin.iloc[0,0] ,dfmin.iloc[0,1]))
