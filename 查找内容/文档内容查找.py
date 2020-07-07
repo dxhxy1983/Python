c1bfdc23bfffbb4fd00dbd32d4537479227a8f37
dxhxy <dxhxy1983@foxmail.com>
2020-07-07 15:50:46 +0800

    增加字段筛选

--- 查找内容/文档内容查找.py
+++ 查找内容/文档内容查找.py
@@ -1,4 +1,4 @@
-f = open("7034.txt",encoding='utf-8')
+f = open("C:\\Users\\D\\Documents\\Python\\查找内容\\0704.txt",encoding='utf-8')
 ls = f.readlines()
 # s = set(ls)
 s=ls
@@ -6,7 +6,7 @@ title = '查找结果' + '.txt'
 
 for i in range(len(s)):
     # if ('"write_data_11"[9]')in s[i] or '"read_data_11"' in s[i] and not 'PLC4' in s[i]:
-      if("PLC_In_Storage_1"in s[i]):
+      if("PLC_OUT_Storage"in s[i] and "2020-07-04" in s[i]):
         with open(title, "a") as f:
             f.write(s[i])
     
