2df22629fc56b147aca33cab3defbf1d8e35c33f
dxhxy1983 <dxhxy1983@foxmail.com>
2021-06-17 22:49:07 +0800

    修改读取文件

--- 日常使用/内部收益率.py
+++ 日常使用/内部收益率.py
@@ -1,18 +1,27 @@
 import math
 
 def main():
-    irr_year=eval(input("请输入收益率："))
-    irr_month=pow((1+irr_year),1/12)-1
-    print(irr_month)
-    n=eval(input("请输入总期数："))
+    # irr_year=eval(input("请输入收益率："))
+    # irr_month=pow((1+irr_year/100.0),1/12)-1
+    # print(irr_month)
+    # n=eval(input("请输入总期数："))
     a=[]
-    sum=0
-    for i in range (0,n):
-        a.append(eval(input("请输入第{0}期数值：".format(i+1))))
-    # print(a)
-    for i in range (0,n):
-        sum=+a[i]*pow((1+irr_month),n-i)
-        print(sum)
+    with open(r"C:\Users\DXHQXX\Documents\GitHub\Python\日常使用\现金流.txt") as f:
+        lines=f.readlines
+        for line in lines:
+            a.append(int(line))
+    print(a)
+
+
+
+    
+    # sum=0
+    # for i in range (0,n):
+    #     a.append(eval(input("请输入第{0}期数值：".format(i+1))))
+    # # print(a)
+    # for i in range (0,n):
+    #     sum=sum+a[i]*pow((1+irr_month),n-i)
+    #     print(sum)
 
 
 if __name__=="__main__":
