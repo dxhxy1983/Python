59116d9e4097828af0b7b614a9f1d825abcd6706
dxhxy1983 <dxhxy1983@foxmail.com>
2021-07-13 23:59:08 +0800

    代码完成，但存在问题

---
+++ PySide/85滚动条控件.py
@@ -0,0 +1,71 @@
+##############
+#滚动条控件（QScrollBar)
+#  
+# QScrollBar的作用
+# 1.通过滚动条值的变化,控制其他控件的状态变化
+# 2.通过滚动条值的变化,控制控件位置的变化
+# ##############*)
+
+
+import sys
+
+
+# from PySide2.QtWidgets import QHBoxLayout
+# from PySide2 import QtWidgets
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+# from PyQt5.QtCore import *
+# from PyQt5.QtGui import *
+# from PyQt5.QtWidgets import *
+
+class ScrollBar(QWidget):
+
+    def __init__(self) :
+        super(ScrollBar,self).__init__()
+        self.initUI()
+    
+    def initUI(self):
+        hbox =QHBoxLayout()
+        self.lable=QLabel('拖动滚动条去改变文字颜色')
+        hbox.addWidget(self.lable)
+
+        self.scrollbar1=QScrollBar() 
+        self.scrollbar1.setMaximum(255)
+        self.scrollbar1.SliderMove.connect(self.sliderMoved)
+        # self.scrollbar1.SliderMove.connect(self.sliderMoved)
+        # self.scrollbar1.sconnect(self.sliderMoved)
+        # self.scrollbar1.SliderValueChange.connect(self.sliderMoved)
+        # self.scrollbar1.move.connect(self.sliderMoved)
+
+
+        self.scrollbar2=QScrollBar()
+        self.scrollbar2.setMaximum(255)
+        self.scrollbar2.SliderMove.connect(self.sliderMoved)
+
+        self.scrollbar3=QScrollBar()
+        self.scrollbar3.setMaximum(255)
+        self.scrollbar3.SliderMove.connect(self.sliderMoved)
+
+        hbox.addWidget(self.scrollbar1)
+        hbox.addWidget(self.scrollbar2)
+        hbox.addWidget(self.scrollbar3)
+
+        self.setGeometry(300,300,300,200)
+
+        self.setLayout(hbox)
+    
+    
+    def sliderMoved(self):
+        print(self.scrollbar1.value(),self.scrollbar2.value(),self.scrollbar1.value())
+        palette=QPalette()
+        c=QColor(self.scrollbar1.value(),self.scrollbar2.value(),self.scrollbar1.value(),255)
+        palette.setColor(QPalette.foreground.c)
+        self.lable.setPalette(palette)
+        
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    demo =ScrollBar()
+    demo.show()
+    sys.exit(app.exec_())
