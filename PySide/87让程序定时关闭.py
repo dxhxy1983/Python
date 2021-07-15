b7b8113488f511282a5d3294159f4d2823e55a19
dxhxy1983 <dxhxy1983@foxmail.com>
2021-07-15 09:15:34 +0800

    程序完成

--- PySide/87让程序定时关闭.py
+++ PySide/87让程序定时关闭.py
@@ -13,10 +13,10 @@ from PySide2.QtWidgets import *
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
-    label=QLabel('<font color=red size=640> <b>Hello World,窗口在5秒后自动关闭!</b></font>')
+    label=QLabel('<font color=red size=140> <b>Hello World,窗口在5秒后自动关闭!</b> </font>')
     label.setWindowFlags(Qt.SplashScreen | Qt.FramelessWindowHint)
     label.show()
-    QTimer.singleShot(5000,app.quit)
+    QTimer.singleShot(1000,app.quit)
 
 
     sys.exit(app.exec_())
