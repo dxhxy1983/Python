24133cec4f47bb3452f7fd25e76d256c26e9852c
dxhxy1983 <dxhxy1983@foxmail.com>
2021-08-03 15:10:36 +0800

    测试完成

---
+++ PySide/90装载本地页面.py
@@ -0,0 +1,35 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtWebEngineWidgets import *
+
+import sys
+import os
+
+class WebEngineView(QMainWindow):
+    def __init__(self):
+        super(WebEngineView,self).__init__()
+        self.setWindowTitle("装载本地Web页面")
+        self.setGeometry(5,30,1355,730)
+              
+        url= os.getcwd()+"/test.html"
+         
+        self.browser=QWebEngineView()
+        self.browser.load(QUrl.fromLocalFile(url))
+
+        self.setCentralWidget(self.browser)
+
+        print(os.getcwd())
+        
+
+
+
+    
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    win=WebEngineView()
+    win.show()
+    sys.exit(app.exec_())
+
+
