a3062168622738a6ad073ee3bd83bdcc64fe0a9e
dxhxy1983 <dxhxy1983@foxmail.com>
2021-08-10 21:34:59 +0800

    修改import

--- PySide/93Javascript调用pythonAPI.py
+++ PySide/93Javascript调用pythonAPI.py
@@ -1,10 +1,10 @@
-from PyQt5.QtWidgets  import *
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import *
-from MySharedObject  import MySharedObject
+# from PyQt5.QtWidgets  import *
+# from PyQt5.QtWebEngineWidgets import QWebEngineView
+# from PyQt5.QtCore import *
+# from MySharedObject  import MySharedObject
 from PyQt5.QtWebChannel import  QWebChannel
-import sys
-import os
+# import sys
+# import os
 
 '''
 
