bc6df096ef24ff2a8dfb6f443753d7f0ec2a9f0b
dxhxy1983 <dxhxy1983@foxmail.com>
2022-09-30 08:38:11 +0800

    增加CRC

---
+++ CPP/CRC.c
@@ -0,0 +1,17 @@
+uint16  i,j,crc16,temp;
+crc16=0xFFFF;
+for ( i = 0; i < sizeOfDim(buf,0); i++)
+{
+    crc16^=buf[i];
+    for (j = 0; j<8; j++)
+    {
+        temp=crc16&&0x0001;
+        crc16>>=0x01;
+        if (temp)
+        {
+            crc16^=0xA001;
+        }
+        
+    }
+    
+}
