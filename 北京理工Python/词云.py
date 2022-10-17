42f22cf25ecbdb0416d0cb40c436597245eaf99a
dxhxy1983 <dxhxy1983@foxmail.com>
2022-10-18 00:32:54 +0800

    停用词修改

--- 北京理工Python/词云.py
+++ 北京理工Python/词云.py
@@ -6,13 +6,17 @@ from matplotlib import get_backend
 from wordcloud import WordCloud
 with open(r'C:\Users\DXHQXX\Documents\gitee\北京理工Python\二十大报告.txt', 'r',encoding="utf_8") as file1:
     txt= file1.read() 
-# stp_words=["同志们","五年来"]  
-jieba.del_word("同志们")
-jieba.del_word("五年来")      
-words = jieba.lcut(txt)     #精确分词
+# txt="这是中国胜利得时候,同志们,我们五年来辛苦了"
+stp_words=["同志","五年","十年",'们','来']  
+   
+words = jieba.cut(txt)     #精确分词
+stayed_line=""
+for word in words:
+    if word not in stp_words:
+        stayed_line+=word
 
-newtxt = ''.join(words)    #空格拼接
-
+# newtxt = ''.join(words)    #空格拼接
+newtxt = ''.join(stayed_line)
 img = Image.open(r"北京理工Python\20.bmp") # 打开遮罩图片
 mask = np.array(img) #将图片转换为数组
 
