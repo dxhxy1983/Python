5bba7df9c6a33734521e59f8a6f53a8b69048c95
dxhxy1983 <dxhxy1983@foxmail.com>
2023-01-13 17:07:19 +0800

    验证码测试中

--- PLC/有效期验证码.py
+++ PLC/有效期验证码.py
@@ -1,4 +1,36 @@
-list=[0,31,61,127,241,487]
-for i in range(1,6):
- j=(i*list[i]*9973)%10000
- print(j)
+
+
+list=[31,61,127,241,487]
+a=[]
+c=0
+# for i in range(10000,99999):
+#     # for j in list:
+#         b=(i*31+9973)%10000
+#         if b>1000:
+#             a.append(b)
+# print(a)
+# # for mod1 in a:
+# #     for mod2 in a:
+# #         for mod3 in a:
+# #             for mod4 in a:
+
+
+mod=3445
+for i in range(10000,99999):
+        for j in list:
+            b=(i*int(j)*9973)//10000
+            if b==mod:
+                # print(mod,i,j)
+                if mod not in a:
+                    a.append(mod)
+                # else:
+                #     print("有重复")
+                    
+                #     print(mod,i,j)
+                #     c=c+1
+                    
+# print(c)
+print(a)
+
+
+
