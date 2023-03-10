264432bf24d1f2450abeb5cc29cea3344d6230ef
dxhxy1983 <dxhxy1983@foxmail.com>
2023-03-10 17:31:29 +0800

    更改为读取配置文件

--- ModbusRTU/Modbus命令转换.py
+++ ModbusRTU/Modbus命令转换.py
@@ -7,6 +7,7 @@ import modbus_tk.defines as cst
 from modbus_tk import modbus_rtu
 import time
 import sys
+import os
 
 
 def mod(PORT,address,FucNum,start_add,num,outputvalue=0):
@@ -35,8 +36,25 @@ def mod(PORT,address,FucNum,start_add,num,outputvalue=0):
 
 
 if __name__ == "__main__":
-    num=0
-    Port="com2"
+    current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    file_path=current_path+"\\settings.txt"
+    parametersList=[]
+    
+    with open(file_path,'r',encoding="utf-8") as f:
+        i=0
+        for line in f:
+            
+            line=line.strip('\n')
+            # print(line)
+            if line!="":
+                key,value=line.split(":")
+                parametersList.append(value)
+      
+    
+    Port=parametersList[0]
+    comAdd1=parametersList[1]
+    FuckNum=parametersList[2]
+    num=parametersList[3]
     # comAdd1=3 #IO板地址
     # #设置工作模式为单步
     # readStatus,alarm=mod(Port,comAdd1,FucNum=5,
@@ -44,10 +62,11 @@ if __name__ == "__main__":
     #                 outputvalue=1   #1：单步，2：多步，3：报错，0：系统参数
     #                 )
     # Port="com6"
-    comAdd1=1 #匀胶协议板地址
+    # comAdd1=5 #匀胶协议板地址
+
     #设置工作模式为单步
-    readStatus,alarm=mod(Port,comAdd1,FucNum=6,
-                    start_add=257,num=6, #工作模式
+    readStatus,alarm=mod(Port,comAdd1,FucNum=FuckNum,
+                    start_add=0,num=num, #工作模式
                     outputvalue=0   #1：单步，2：多步，3：报错，0：系统参数
                     )
         
