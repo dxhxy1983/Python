ca70c1512fa7a55ef83bd7571492c9bc3cfaf56d
dxhxy1983 <dxhxy1983@foxmail.com>
2023-03-11 20:37:21 +0800

    测试功能完成

---
+++ 日常使用/列出文件夹下所有文件.py
@@ -0,0 +1,71 @@
+import os,sys
+import pandas as pd
+import shutil
+def DFS_file_search(dict_name):
+    
+    # list.pop() list.append()这两个方法就可以实现栈维护功能
+    stack = []
+    result_txt = []
+    stack.append(dict_name)
+    while len(stack) != 0:  # 栈空代表所有目录均已完成访问
+        temp_name = stack.pop()
+        try:
+            temp_name2 = os.listdir(temp_name) # list ["","",...]
+            for eve in temp_name2:
+                stack.append(temp_name + "\\" + eve)  # 维持绝对路径的表达
+        except NotADirectoryError:
+            result_txt.append(temp_name)
+    return result_txt
+
+
+if __name__=="__main__":
+    #获取当前文件目录
+    current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    file_path=current_path+"\\settings.txt"
+    # print(current_path)
+    parametersList=[]
+    
+    with open(file_path,'r',encoding="utf-8") as f:
+        line=f.readlines()
+        for i in range(len(line)):
+            line[i]=line[i].strip('\n')
+            parametersList.append(line[i])
+
+    #获得查找源目录
+    sourceList=DFS_file_search(parametersList[0])
+    #获得要查找的文件名
+    IO=current_path+"\\文件查找.xlsx"
+    readedExcel=pd.read_excel(io=IO)
+    df = pd.DataFrame(readedExcel)
+    # print(df)
+    fileName=[]
+    for index, row in df.iterrows():
+        # print(row[1]+"-"+row[0])
+        fileName.append(str(row[1])+"-"+str(row[0]))
+
+    #获得要找的文件路径
+    listFilePath=[]
+    listFailed=[]
+    #获得查到的文件路径
+    for file in fileName:
+        for i in range(len(sourceList)):
+            if file in sourceList[i]:
+                listFilePath.append(sourceList[i])
+                fileName.remove(file)
+
+    # print(listFilePath)
+    dstpath=current_path+"\\日常使用"
+    #复制文件到指定目录
+    for fPath in listFilePath:
+        fpath,fname=os.path.split(fPath)
+        shutil.copy(fPath, dstpath + fname)
+    file_path=current_path+"\\查找失败的文件.txt"
+    with open(file_path,'a',encoding="utf-8") as f:
+        for i in fileName:
+            f.write(i)
+    f.close()
+
+
+
+
+
