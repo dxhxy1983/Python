19f00dca0a6264fa1a715978e755c358f8fee146
dxhxy1983 <dxhxy1983@foxmail.com>
2024-02-17 16:56:48 +0800

    完成头文件转为excel文件

--- 日常使用/头文件转换excel.py
+++ 日常使用/头文件转换excel.py
@@ -1,4 +1,8 @@
-fPath=r"F:\myDoc\github\Python\日常使用\add.txt"
+import openpyxl
+workbook=openpyxl.Workbook()
+sheet=workbook.active
+# fPath=r"F:\myDoc\github\Python\日常使用\add.txt"
+fPath=r"C:\Users\DXHQXX\Documents\gitee\日常使用\add.txt"
 
 try:
     f = open(fPath)
@@ -9,12 +13,19 @@ ls = f.readlines()
 # ls = ls[::-1] #内容倒序
 
 lt = []
+i=0
 for item in ls:
+    i=i+1
     item = item.strip("\n")
     # item = item.replace(" ", "")
     lt = item.split(" ")
     # print(lt)
     # lt = lt[::-1]
-    print(",".join(lt))
+    for j in range(len(lt)):
+        sheet.cell(row=i,column=j+1).value=lt[j]
+        
+workbook.save("output.xlsx")
+workbook.close()
+
     
 f.close()
